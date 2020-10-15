c53e571c68f0caef574a4f1f66833bc858909c3a
Anup Bagali <anup.bagali1@gmail.com>
2020-10-15 00:41:56 -0400

    add servo code

--- src/fs/targets/static_fire.cpp
+++ src/fs/targets/static_fire.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <Servo.h>
 #include "SoftwareSerial.h"
+
 #define NEWLINE 13
 #define STOP 999
-SoftwareSerial XBee(2, 3);
+#define ESC_PIN 9
+#define MIN_THROTTLE 1000
+#define MAX_THROTTLE 2000
+
 String buf;
 bool exited = false;
+
+Servo servo;
+SoftwareSerial XBee(2, 3);
+
+void arm(){
+    servo.writeMicroseconds(0);
+    delay(3000);
+    servo.writeMicroseconds(MAX_THROTTLE);
+    delay(3000);
+    servo.writeMicroseconds(MIN_THROTTLE);
+    delay(5000);
+}
+
 void setup()
 {
-  XBee.begin(9600);
-  Serial.begin(15200);
-  Serial.println("teensy started");
-  /*
-  arm
-  */
+    servo.attach(ESC_PIN);
+    delay(1000);
+    arm();
+
+    XBee.begin(9600);
+    Serial.begin(15200);
+    Serial.println("teensy started");
 }
 
 void loop()
@@ -27,17 +46,13 @@ void loop()
             buf += (char)inByte; // converts to ascii char
             if(inByte == NEWLINE){
                 float throttle = buf.toFloat();
-                if(throttle == 999){
-                    /*
-                    abort/stop
-                    */
+                if(throttle == 999){ // abort/stop 
+                    servo.writeMicroseconds(0);
                     XBee.write("exited\n");
                     exited = true;
                     return;
                 }
-                /*
-                write to esc
-                */
+                servo.writeMicroseconds(throttle/100.0 * MAX_THROTTLE); // write value
                 XBee.write(String("wrote: " + String(throttle) + "\n").c_str());
                 buf = "";
                 Serial.println(throttle);
