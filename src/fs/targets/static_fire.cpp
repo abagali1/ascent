1767b5d3c31438e671929d76b53b40549a71d7b2
Anup Bagali <anup.bagali1@gmail.com>
2020-10-15 00:35:35 -0400

    comment blocks

--- src/fs/targets/static_fire.cpp
+++ src/fs/targets/static_fire.cpp
@@ -10,30 +10,39 @@ void setup()
   XBee.begin(9600);
   Serial.begin(15200);
   Serial.println("teensy started");
+  /*
+  arm
+  */
 }
 
 void loop()
 {
-  if(!exited){
-    while (XBee.available() > 0) {
-      byte inByte = XBee.read(); // reads into ascii decimal
-      if(inByte != NEWLINE && (inByte > 57 || inByte < 48)){ // not a number
-        buf = "";
-        continue;
-      }
-      buf += (char)inByte; // converts to ascii char
-      if(inByte == NEWLINE){
-        float throttle = buf.toFloat();
-        if(throttle == 999){
-          XBee.write("exited\n");
-          exited = true;
-          return;
+    if(!exited){
+        while (XBee.available() > 0) {
+            byte inByte = XBee.read(); // reads into ascii decimal
+            if(inByte != NEWLINE && (inByte > 57 || inByte < 48)){ // not a number
+                buf = "";
+                continue;
+            }
+            buf += (char)inByte; // converts to ascii char
+            if(inByte == NEWLINE){
+                float throttle = buf.toFloat();
+                if(throttle == 999){
+                    /*
+                    abort/stop
+                    */
+                    XBee.write("exited\n");
+                    exited = true;
+                    return;
+                }
+                /*
+                write to esc
+                */
+                XBee.write(String("wrote: " + String(throttle) + "\n").c_str());
+                buf = "";
+                Serial.println(throttle);
+            }
         }
-        XBee.write(String("wrote: " + String(throttle) + "\n").c_str());
-        buf = "";
-        Serial.println(throttle);
-      }
+        delay(500);
     }
-    delay(500);
-  }
 }
