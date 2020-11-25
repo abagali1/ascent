f08ed08e88a60176bf0171f62e284a17db21c090
Ankit Khandelwal <amtron521@gmail.com>
2020-11-24 23:54:59 -0500

    kalman predict method, kinda sped with matrix math lib but it is what it is

--- src/fs/targets/kalman_test.cpp
+++ src/fs/targets/kalman_test.cpp
@@ -12,17 +12,17 @@ Adafruit_BMP3XX bmp;
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 double previous_clock_time;
 double dt = 1 / 100;
-mtx_type F[2][2] = {{1, dt}, {0, 1}}
+mtx_type F[2][2] = {{1, dt}, {0, 1}};
 mtx_type x[2][1] = {{0}, {0}};
-mtx_type B[2][2] = {{0.5*dt*dt, 0}, {0, dt}}
-mtx_type u[2][1] = {{0}, {0}} //0s for initialization?
+mtx_type B[2][2] = {{0.5*dt*dt, 0}, {0, dt}};
+mtx_type u[2][1] = {{0}, {0}}; //0s for initialization?
 mtx_type FT[2][2];
-Matrix.Transpose((mtx_type*)F, 2, 2, FT)
+Matrix.Transpose((mtx_type*)F, 2, 2, FT);
 double x_var = 0;
 double vx_var = 0;
-mtx_type P[2][2] = {{x_var, 0}, {0, vx_var}}
-double Q_var = 2.35; //Process Noise Variance
-Gaussian Q = Gaussian(0, Q_var)
+mtx_type P[2][2] = {{x_var, 0}, {0, vx_var}};
+double Q_var = 2.35;
+
 
 void setup() {
   Wire.begin(); 
@@ -76,6 +76,21 @@ void loop() {
 //   delay(1000);
 }
 
+mtx_type kalman_predict(){
+  mtx_type Fx;
+  Matrix.Multiply((mtx_type*)F, (mtx_type*)x, 2, 2, 1, Fx);
+  mtx_type Bu;
+  Matrix.Multiply((mtx_type*)B, (mtx_type*)u, 2, 2, 1, Bu);
+  mtx_type prior;
+  Matrix.Add((mtx_type*)Fx, (mtx_type*)Bu, 2, 1, prior);
+  
+  mtx_type FP;
+  Matrix.Multiply((mtx_type*)F, (mtx_type*)P, 2, 2, 2, FP);
+  mtx_type FPFT;
+  Matrix.Multiply((mtx_type*)FP, (mtx_type*)FT, 2, 2, 2, FPFT);
+  Matrix.Add((mtx_type*)FPFT, (mtx_type*)Q, 2, 2, P);
+  return prior;
+}
 void process_noise(double mean, double variance){
   double [2][2] to_return;
   for(int i = 0; i<2; i++){
