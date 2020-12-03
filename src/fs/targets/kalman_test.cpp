2c0ef83f80c2b3ed5d0b228e3273649719ff70a8
Anup Bagali <anup.bagali1@gmail.com>
2020-12-03 15:20:26 -0500

    iniitla kalmn

--- src/fs/targets/kalman_test.cpp
+++ src/fs/targets/kalman_test.cpp
@@ -3,8 +3,7 @@
 #include <Adafruit_BMP3XX.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-#include <MatrixMath.h>
-#include <Gaussian.h>
+#include <lin/core.hpp>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
@@ -13,17 +12,13 @@ Adafruit_BMP3XX bmp;
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 double previous_clock_time;
 double dt = 1 / 100;
-mtx_type F[2][2] = {{1, dt}, {0, 1}};
-mtx_type x[2][1] = {{0}, {0}};
-mtx_type B[2][2] = {{0.5*dt*dt, 0}, {0, dt}};
-mtx_type u[2][1] = {{0}, {0}}; //0s for initialization?
-mtx_type FT[2][2];
-Matrix.Transpose((mtx_type*)F, 2, 2, FT);
 double x_var = 0;
 double vx_var = 0;
-mtx_type P[2][2] = {{x_var, 0}, {0, vx_var}};
 double Q_var = 2.35;
 float z_o = 0;
+lin::Vector2d x = {0.0, 0.0};
+lin::Matrix2x2d P = {0.2, 0.0, 1.2, 0.0};
+lin::Matrix2x2d Q = {0.001, 0.1, 0.1, 0.001};
 
 
 void setup() {
@@ -61,36 +56,18 @@ void loop() {
   float z = bmp.readAltitude(SEALEVELPRESSURE_HPA) - z_o;
 
   double clock_time = millis();
+  float dt = 1e-2;
+  lin::Matrix2x2d F = {1.0, dt, 0.0, 1.0};
+  lin::Vector2d B = {0.5*dt*dt, dt};
+
+  lin::Vector2d x_bar = F*x + B*imu_acc_reading.z();
+  P = F*P*lin::transpose(F) + Q;
+  lin::Vector2d H = {1.0, 0.0};
 
 
 
-  delay((period * 1000) - clock_time - previous_clock_time);
+
+  // delay((period * 1000) - clock_time - previous_clock_time);
   previous_clock_time = clock_time;
 //   delay(1000);
-}
-
-mtx_type kalman_predict(){
-  mtx_type Fx;
-  Matrix.Multiply(&F, &x, 2, 2, 1, Fx);
-  mtx_type Bu;
-  Matrix.Multiply(&B, &u, 2, 2, 1, Bu);
-  mtx_type prior;
-  Matrix.Add(&Fx, &Bu, 2, 1, prior);
-  
-  mtx_type FP;
-  Matrix.Multiply(&F, &P, 2, 2, 2, FP);
-  mtx_type FPFT;
-  Matrix.Multiply(&FP, &FT, 2, 2, 2, FPFT);
-  Matrix.Add(&FPFT, &Q, 2, 2, P);
-  return prior;
-}
-void process_noise(double mean, double variance){
-  double [2][2] to_return;
-  for(int i = 0; i<2; i++){
-    for(int j = 0; j<2; j++){
-      double x = random(0, 999)/1000*random(-3*variance, 3*variance)
-      to_return [i][j] = (1/sqrt(2*variance*pi))*(exp((-1*pow(x-mean, 2))/(2*variance))) 
-    }
-  }
-  return to_return
 }
