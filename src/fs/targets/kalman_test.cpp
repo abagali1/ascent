9bfb1194fd4d74dc775b9781431e6d413a2ff35c
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2020-12-26 17:15:37 -0500

    Update kalman_test.cpp

--- src/fs/targets/kalman_test.cpp
+++ src/fs/targets/kalman_test.cpp
@@ -3,7 +3,6 @@
 #include <Adafruit_BMP3XX.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-#include <lin/core.hpp>
 #include <SD.h>
 #include <matrix.hpp>
 
@@ -20,9 +19,13 @@ File fout;
 
 int i = 0;
 float z_o = 0;
-lin::Vector2d x = {0.0, 0.0};
-lin::Matrix2x2d P = {0.2, 0.0, 1.2, 0.0};
-lin::Matrixd<1,1> R = {4};
+Matrix x(2,1);
+Matrix P(2,2);
+Matrix Q(2,2);
+Matrix R(1,1);
+Matrix f(2,2);
+Matrix B(2,1);
+Matrix H(1,2);
 
 void setup() {
   Wire.begin(); 
@@ -65,8 +68,10 @@ void setup() {
   }
 
   delay(1000);
+
   Serial.println("initial reading");
   bmp.readAltitude(SEALEVELPRESSURE_HPA);
+
   Serial.println("reading");
   fout = SD.open("init.txt", FILE_WRITE);
   for(int j=0;j<1000;j++){
@@ -81,25 +86,50 @@ void setup() {
   fout.close();
   fout = SD.open("data.txt", FILE_WRITE);
   bno.setExtCrystalUse(true);
-  previous_clock_time = millis();
+
   Serial.println("init successsss");
+
+  x.fill(std::vector<float>({0.0, 0.0}));
+  P.fill(std::vector<float>({0.2, 0, 0, 1.2}));
+  Q.fill(std::vector<float>({0.001, 0.1, 0.1, 0.001}));
+  R.fill(std::vector<float>({4.0}));
+  f.fill(std::vector<float>({1, dt, 0, 1}));
+  B.fill(std::vector<float>({0.5 * dt * dt, dt}));
+  H.fill(std::vector<float>({1,0}));
+
+  pinMode(1, OUTPUT);
+  
+  digitalWrite(1, HIGH);
   delay(1000);
+  digitalWrite(1, LOW);
+  
+  delay(10000);
+
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(1, LOW);
+
+  previous_clock_time = millis();
 }
 
 void loop() {
   imu::Vector<3> imu_acc_reading = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  float z = bmp.readAltitude(SEALEVELPRESSURE_HPA) - z_o;
   float u = imu_acc_reading.z();
   float clock_time = millis();
-  fout.printf("0,%e,%e\n", u, z); // dt, az, z
+
+  Matrix z(1,1);
+  z.fill(std::vector<float>({bmp.readAltitude(SEALEVELPRESSURE_HPA) - z_o}));
+
+  Matrix x_bar = (f * x) + (B * u);
+  Matrix P_bar = ((f * P) * f.transpose()) + Q;
+
+  Matrix y = z - (H * x_bar);
+  Matrix S = (H * P_bar * H.transpose()) + R;
+  Matrix K = P_bar * H.transpose() * S.inverse();
+  P = P_bar - (K * H * P_bar);
+  x = x_bar + K * y;
+
+  Serial.println(x.at(0,0));
 
   delay((dt * 1000) - (clock_time - previous_clock_time));
-  previous_clock_time = clock_time;
-  Serial.println(i);
-  if(i >= MAX){
-    fout.close();
-    Serial.println("test finished");
-    while(true){}
-  }
-  i++;
 }
