dd862ee7653ddc1421c2a54143007f20713e7752
Anup Bagali <anup.bagali1@gmail.com>
2020-12-27 01:24:43 -0500

    remove duplicate definition

--- src/fs/FCCode/ClockManager.hpp
+++ src/fs/FCCode/ClockManager.hpp
@@ -9,26 +9,9 @@ class ClockManager: public TimedControlTask{
         ReadableStateField<uint> control_cycle_count_f;
         system_time_t initial_control_cycle_start_time;
         uint control_cycle_total_time;
-
     public:
-        ClockManager(StateFieldRegistry& registry, uint control_cycle_time)
-            :   TimedControlTask(registry, "clockmanager_ct", 0),
-                control_cycle_count_f("control_cycle_count", 0),
-                control_cycle_total_time(control_cycle_time)
-
-        {
-            this->add_readable_field(this->control_cycle_count_f);
-            this->initial_control_cycle_start_time = this->get_current_system_time();
-        }
-
-        void execute(){
-            if(this->control_cycle_count_f.get_value() == 0){
-                system_time_t start_time = TimedControlTaskBase::current_cycle_start_time + this->control_cycle_total_time;
-                this->sleep_until_time(start_time);
-            }
-            TimedControlTaskBase::current_cycle_start_time = this->get_current_system_time();
-            this->control_cycle_count_f.set_value(this->control_cycle_count_f.get_value() + 1);
-        }
+        ClockManager(StateFieldRegistry&, uint);
+        void execute(void);
 };
 
 #endif
