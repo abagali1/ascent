a94592115c7a0a5237bd0601a5aa9e03c1b3e071
sparthaje <35978578+sparthaje@users.noreply.github.com>
2020-12-28 02:34:44 -0500

    groundstation

---
+++ groundstation/src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <div class="main-panel">
+      <dashboard-content>
+      </dashboard-content>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+
+import DashboardContent from "./Content.vue";
+export default {
+  components: {
+    DashboardContent,
+  },
+  methods: {
+  }
+};
+</script>
---
+++ groundstation/src/pages/Dashboard.vue
@@ -0,0 +1,300 @@
+<template>
+  <div>
+
+    <div class="row lshift">
+      <div class="col-lg-3" :class="{'text-right': isRTL}">
+        <card type="chart" class="shrink">
+          <template slot="header">
+            <h3 class="card-title">Estimated Altitude (m)</h3>
+            <h5 class="card-category">Current Altitude - 5 m</h5>
+          </template>
+          <div class="chart-area">
+            <line-chart style="height: 100%"
+                        chart-id="purple-line-chart"
+                        :chart-data="purpleLineChart.chartData"
+                        :gradient-colors="purpleLineChart.gradientColors"
+                        :gradient-stops="purpleLineChart.gradientStops"
+                        :extra-options="purpleLineChart.extraOptions">
+            </line-chart>
+          </div>
+        </card>
+        <card type="chart" class="shrink" style="margin-top: -1em;">
+          <template slot="header">
+            <h3 class="card-title">Estimated Velocity z (ms^-1)</h3>
+            <h5 class="card-category">Current Velocity - 0.23 ms^-1</h5>
+          </template>
+          <div class="chart-area">
+            <line-chart style="height: 100%"
+                        chart-id="purple-line-chart"
+                        :chart-data="purpleLineChart.chartData"
+                        :gradient-colors="purpleLineChart.gradientColors"
+                        :gradient-stops="purpleLineChart.gradientStops"
+                        :extra-options="purpleLineChart.extraOptions">
+            </line-chart>
+          </div>
+        </card>
+        <card type="chart" class="shrink" style="margin-top: -1em;">
+          <template slot="header">
+            <h3 class="card-title">Acceleration z (ms^-2)</h3>
+            <h5 class="card-category">Current Acceleration - 0.23 ms^-2</h5>
+          </template>
+          <div class="chart-area">
+            <line-chart style="height: 100%"
+                        chart-id="purple-line-chart"
+                        :chart-data="purpleLineChart.chartData"
+                        :gradient-colors="purpleLineChart.gradientColors"
+                        :gradient-stops="purpleLineChart.gradientStops"
+                        :extra-options="purpleLineChart.extraOptions">
+            </line-chart>
+          </div>
+        </card>
+      </div>
+      <div class="col-lg-3" :class="{'text-right': isRTL}">
+        <card type="chart" class="shrink">
+          <template slot="header">
+            <h3 class="card-title">Yaw (degrees)</h3>
+            <h5 class="card-category">Current Yaw - 0.23 deg</h5>
+          </template>
+          <div class="chart-area">
+            <line-chart style="height: 100%"
+                        chart-id="purple-line-chart"
+                        :chart-data="purpleLineChart.chartData"
+                        :gradient-colors="purpleLineChart.gradientColors"
+                        :gradient-stops="purpleLineChart.gradientStops"
+                        :extra-options="purpleLineChart.extraOptions">
+            </line-chart>
+          </div>
+        </card>
+      </div>
+      <div class="col-lg-3" :class="{'text-right': isRTL}">
+        <card type="chart" class="shrink">
+          <template slot="header">
+            <h3 class="card-title">Pitch (degrees)</h3>
+            <h5 class="card-category">Current Pitch - 0.23 deg</h5>
+          </template>
+          <div class="chart-area">
+            <line-chart style="height: 100%"
+                        chart-id="purple-line-chart"
+                        :chart-data="purpleLineChart.chartData"
+                        :gradient-colors="purpleLineChart.gradientColors"
+                        :gradient-stops="purpleLineChart.gradientStops"
+                        :extra-options="purpleLineChart.extraOptions">
+            </line-chart>
+          </div>
+        </card>
+      </div>
+      <div class="col-lg-3" :class="{'text-right': isRTL}">
+        <card type="chart" class="shrink">
+          <template slot="header">
+             <h3 class="card-title">Roll (degrees)</h3>
+            <h5 class="card-category">Current Roll - 0.23 deg</h5>
+          </template>
+          <div class="chart-area">
+            <line-chart style="height: 100%"
+                        chart-id="green-line-chart"
+                        :chart-data="greenLineChart.chartData"
+                        :gradient-stops="greenLineChart.gradientStops"
+                        :extra-options="greenLineChart.extraOptions">
+            </line-chart>
+          </div>
+        </card>
+      </div>
+    </div>
+    
+  </div>
+</template>
+<script>
+  import LineChart from '@/components/Charts/LineChart';
+  import BarChart from '@/components/Charts/BarChart';
+  import * as chartConfigs from '@/components/Charts/config';
+  import TaskList from './Dashboard/TaskList';
+  import UserTable from './Dashboard/UserTable';
+  import config from '@/config';
+
+  export default {
+    components: {
+      LineChart,
+      BarChart,
+      TaskList,
+      UserTable
+    },
+    data() {
+      return {
+        purpleLineChart: {
+          extraOptions: chartConfigs.purpleChartOptions,
+          chartData: {
+            labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+            datasets: [{
+              label: "Data",
+              fill: true,
+              borderColor: config.colors.primary,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.primary,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.primary,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+            }, 
+            {
+              label: "My First dataset",
+              fill: true,
+              borderColor: config.colors.danger,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.danger,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.danger,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: [30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
+            }
+            ]
+          },
+          gradientColors: config.colors.primaryGradient,
+          gradientStops: [1, 0.2, 0],
+        },
+        greenLineChart: {
+          extraOptions: chartConfigs.greenChartOptions,
+          chartData: {
+            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
+            datasets: [{
+              label: "My First dataset",
+              fill: true,
+              borderColor: config.colors.danger,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.danger,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.danger,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: [90, 27, 60, 12, 80],
+            }, 
+            {
+              label: "My First dataset",
+              fill: true,
+              borderColor: config.colors.danger,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.danger,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.danger,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: [30, 30, 30, 30, 30],
+            }
+            
+            ]
+          },
+          gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
+          gradientStops: [1, 0.4, 0],
+        },
+        blueBarChart: {
+          extraOptions: chartConfigs.barChartOptions,
+          chartData: {
+            labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+            datasets: [{
+              label: "Countries",
+              fill: true,
+              borderColor: config.colors.info,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              data: [53, 20, 10, 80, 100, 45],
+            }]
+          },
+          gradientColors: config.colors.primaryGradient,
+          gradientStops: [1, 0.4, 0],
+        }
+      }
+    },
+    computed: {
+      enableRTL() {
+        return this.$route.query.enableRTL;
+      },
+      isRTL() {
+        return this.$rtl.isRTL;
+      },
+      bigLineChartCategories() {
+        return this.$t('dashboard.chartCategories');
+      }
+    },
+    methods: {
+      initBigChart(index) {
+        let chartData = {
+          datasets: [{
+            fill: true,
+            borderColor: config.colors.primary,
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: config.colors.primary,
+            pointBorderColor: 'rgba(255,255,255,0)',
+            pointHoverBackgroundColor: config.colors.primary,
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: this.bigLineChart.allData[index]
+          }],
+          labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+        }
+        this.$refs.bigChart.updateGradients(chartData);
+        this.bigLineChart.chartData = chartData;
+        this.bigLineChart.activeIndex = index;
+      }
+    },
+    trigger_launch() {
+
+    },
+    abort_ground() {
+
+    },
+    abort_flight() {
+
+    },
+    calibrate_imu() {
+
+    },
+    calibrate_altimeter() {
+
+    },
+    mounted() {
+      this.i18n = this.$i18n;
+      if (this.enableRTL) {
+        this.i18n.locale = 'ar';
+        this.$rtl.enableRTL();
+      }
+      this.initBigChart(0);
+    },
+    beforeDestroy() {
+      if (this.$rtl.isRTL) {
+        this.i18n.locale = 'en';
+        this.$rtl.disableRTL();
+      }
+    }
+  };
+</script>
+<style>
+.lshift {
+  margin-left:-20em;
+  margin-top: -5em;
+}
+.shrink {
+  height:22.6em;;
+}
+</style>
