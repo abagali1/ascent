f325aa52fd1a4b43b0ef8c722d2276d8bcaa7333
sparthaje <35978578+sparthaje@users.noreply.github.com>
2021-01-24 17:26:01 -0500

    Update matrix_test.cpp

--- src/fs/targets/matrix_test.cpp
+++ src/fs/targets/matrix_test.cpp
@@ -1,27 +1,12 @@
 #include <Arduino.h>
-#include "matrix.hpp"
+#include <lin/core.hpp>
 #include <vector>
-
-Matrix x(2,1);
-Matrix b(2,1);
-Matrix f(2,2);
 
 void setup(){
     Serial.begin(9600);
-    x.fill(std::vector<float>({1, 2}));
-    b.fill(std::vector<float>({3, 4}));
-    f.fill(std::vector<float>({1, 0, 0, 1}));
     Serial.println("inited");
     delay(1000);
 }
 
 void loop(){
-    Matrix r = x + b;
-    Serial.println(r.matrixData.at(0));
-    Serial.println(r.matrixData.at(1));
-    Serial.println();
-    r = f * x;
-    Serial.println(r.matrixData.at(0));
-    Serial.println(r.matrixData.at(1));
-    Serial.println("----------");
 }
